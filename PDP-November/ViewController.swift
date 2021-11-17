a00b7876f318c403f1d67cc09fde7fe7c89b3147
Mikhail Kisly <mikhail.kisly@itrexgroup.com>
2021-11-17 16:35:04 +0300

    Initial Commit

---
+++ PDP-November/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PDP-November
+//
+//  Created by Mikhail Kisly on 17.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
